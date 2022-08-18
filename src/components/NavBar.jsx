cb43c4dc8027aeef2d30456ceb72256cb722a5e3
Marcos Iannibelli <mdiannibelli@gmail.com>
2022-08-18 20:21:50 -0300

    bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function NavBar() {
   return (
 
