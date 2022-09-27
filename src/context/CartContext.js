16fd9fb0bc240dd436f3c6ca40fe4b13b64e2a64
Marcos Iannibelli <mdiannibelli@gmail.com>
2022-09-27 13:49:06 -0300

    localstorage

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,9 +1,18 @@
-import {createContext, useContext, useState } from "react";
+import {createContext, useContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
+const productsFromLocalStorage = JSON.parse(localStorage.getItem('cart') || '[]')
+
 export const CartProvider = ({children}) => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(productsFromLocalStorage);
+
+    useEffect (() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+
+
     const addItem = (item) => {
         
         const existInCart = cart.find((product) => product.id === item.id)
